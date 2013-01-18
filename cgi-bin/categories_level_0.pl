582a91bd80f20e5f2ff5ff1e3c569eec38ac586d
Devesh Agrawal <devsvits@gmail.com>
2013-01-18 07:07:49 +0530

    adding ajax concepts

--- cgi-bin/categories_level_0.pl
+++ cgi-bin/categories_level_0.pl
@@ -7,6 +7,13 @@ use strict;
 use warnings;
 use HTML::Template;
 use DBI;
+#use CGI::Carp qw(warningsToBrowser fatalsToBrowser);
+use CGI;
+# read the CGI params
+my $cgi = CGI->new;
+my $cate_id = $cgi->param("category_id");
+
+
 #send the obligatory Content-Type
 print "Content-Type: text/html\n\n"; 
   
@@ -39,14 +46,61 @@ while (my @data = $statement->fetchrow_array())
 
 	
 	$row_data {clientName} = $data[1]; 	
-	$row_data {URL}="http://localhost/cgi-bin/showClientBills.cgi?clientid=$data[0]";	
+	$row_data {URL}="http://localhost/cgi-bin/categories_level_0.pl?category_id=$data[0]";
+
+	
 
 	push (@loop_data, \%row_data)	;
 	#print $data[1];
 }
   	 
 $template->param (DB_LOOP => \@loop_data);
+
+$sql_query = "select * from table_categories_level_1 where category_id = '$cate_id'";
+#prepare your statement for connecting to the databse
+$statement = $db_handle->prepare ($sql_query) or die "Couldn't prepare query '$sql_query': $DBI::errstr\n";  
+#execute youe sql statement
+$statement->execute() or die "SQL Error: $DBI::errstr\n";  
+  	 	
+  
+  
+my @loop_data = ();            # initialize an array to hold your loop
+
+while (my @data = $statement->fetchrow_array())
+{
+    my %row_data;               # get a fresh hash for the row data
+    # Fill in this row
+    $row_data {sub_cate_name} = $data[1]; 
+    
+	$sql_query = "select * from table_product where sub_cate_id = '$data[0]'";
+	my $statement2 = $db_handle->prepare ($sql_query) or die "Couldn't prepare query '$sql_query': $DBI::errstr\n";  
+	$statement2->execute() or die "SQL Error: $DBI::errstr\n";  
+	
+	
+    # Here goes the inner loop data
+    my @inner_loop_data = ();            # initialize an array to hold your loop
+    while (my @data2 = $statement2->fetchrow_array())
+	
+    {
+        my %nested_row_data;               # get a fresh hash for the nested row data
+        $nested_row_data{product_name} = $data2[1];
+        # The crucial step - push a reference to this row into the inner loop!
+        push(@inner_loop_data, \%nested_row_data);
+    }
+    $row_data{product_loop} = \@inner_loop_data;
+    # The crucial step - push a reference to this row into the loop!
+    push(@loop_data, \%row_data);
+}
+
+# finally, assign the loop data to the loop param, again with a reference:
+$template->param(DB_LOOP2 => \@loop_data);
+  
+		
+	
+  	 
+
   	
+
 # print the template
 print $template->output;
 $db_handle->disconnect; 
--- cgi-bin/categories_level_1.pl
+++ cgi-bin/categories_level_1.pl
@@ -7,9 +7,12 @@ use strict;
 use warnings;
 use HTML::Template;
 use DBI;
-#send the obligatory Content-Type
-print "Content-Type: text/html\n\n"; 
-  
+use CGI::Carp qw(warningsToBrowser fatalsToBrowser);
+use CGI;
+
+# read the CGI params
+my $cgi = CGI->new;
+
 my $user = 'root';
 my $password = 'Devesh#1';
   
@@ -17,36 +20,42 @@ my $db_handle = DBI->connect ('DBI:mysql:database=qshop',$user,$password)
 	or die "Couldn't connect to database: $DBI::errstr\n";
   		
 #set the value of SQL query
-my $sql_query = "select * from table_categories_level_0 order by category_name";
+my $sql_query = "select sub_cate_name from table_categories_level_1";
 
 #prepare your statement for connecting to the databse
 my $statement = $db_handle->prepare ($sql_query)
 	or die "Couldn't prepare query '$sql_query': $DBI::errstr\n";  
   
 #execute youe sql statement
-$statement->execute()
-	or die "SQL Error: $DBI::errstr\n";  
-  	 	
-# open the HTML template
-my $template = HTML::Template->new(filename => 'categories_level_0.tmpl');
-  
+$statement->execute()  or die "SQL Error: $DBI::errstr\n";  
+  	 
+
 my @loop_data = ();
-  	
 while (my @data = $statement->fetchrow_array())
 {
 	#print @data;		
 	my %row_data; # get a fresh hash for row data
 
 	
-	$row_data {clientName} = $data[1]; 	
-	$row_data {URL}="http://localhost/cgi-bin/showClientBills.cgi?clientid=$data[0]";	
+	$row_data {sub_cate_name} = $data[1]; 	
+	#$row_data {URL}="http://localhost/cgi-bin/showClientBills.cgi?clientid=$data[0]";	
 
 	push (@loop_data, \%row_data)	;
 	#print $data[1];
 }
-  	 
-$template->param (DB_LOOP => \@loop_data);
-  	
-# print the template
+
+# open the HTML template
+my $template = HTML::Template->new(filename => 'categories_level_0.tmpl');
+
+$template->param(BOOL => 1);
+$template->param (DB_LOOP2 => \@loop_data);
+
 print $template->output;
-$db_handle->disconnect; 
+# return JSON string
+
+# create a JSON string according to the database result
+my $json = qq{{"success" : "login is successful", "userid" : @loop_data}} ;
+
+
+print $cgi->header(-type => "application/json", -charset => "utf-8");
+print $json;
